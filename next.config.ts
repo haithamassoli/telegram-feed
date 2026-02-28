b57568b79326b304d90d3dceda0c256744b0f2e3
haithamassoli <haitham.b.assoli@gmail.com>
2026-02-28 04:38:38 +0300

    ignoreBuildErrors: true,

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   turbopack: {
     resolveAlias: {
       net: { browser: "./lib/empty-module.ts" },
